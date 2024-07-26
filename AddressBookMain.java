61caf58a9466e7870830ffec3f117804fa2442e4
shindesurajjotiram <suraj.jotiram@godigit.com>
2024-07-26 14:34:25 +0530

    the main welcome page of address book project is added

---
+++ AddressBookMain.java
@@ -0,0 +1,57 @@
+package addressbooksystem;
+
+import java.util.Scanner;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        AddressBook addressBook = new AddressBook();
+
+        int i;
+        do {
+            System.out.print("\n\n");
+            System.out.println("===========================================================================");
+            System.out.print("\n");
+            System.out.println("Welcome to Address Book");
+            System.out.print("\n\n");
+
+            addressBook.showDetails();
+
+            System.out.print("\n\n");
+            System.out.println("===========================================================================");
+            System.out.print("\n\n");
+
+            System.out.println("Enter 1 to add a new person to Address Book.");
+            System.out.println("Enter 2 to edit existing person using their name.");
+            System.out.println("Enter 3 to delete a person using their name.");
+            System.out.println("Enter 4 to add multiple person to Address Book.");
+            System.out.println("Enter 5 ");
+            System.out.println("Enter 6 to exit.");
+            System.out.println("\n");
+            System.out.print("Enter your option here : ");
+            i = scanner.nextInt();
+
+            switch (i) {
+                case 1:
+                    addressBook.addPerson();
+                    break;
+                case 2:
+                    addressBook.editPerson();
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                default:
+                    System.out.println("Enter the given options only.\n");
+
+
+            }
+
+
+        } while (i != 5);
+
+
+    }
+}
